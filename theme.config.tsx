03cddd194f16d8de800c169ad061c3ed29729ce7
JianyueHugo <valor@jianyuelab.cc>
2023-12-07 18:13:35 +0900

    2023.12.07 1

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>LevelledMobs WIKI</span>,
+  project: {
+    link: 'https://github.com/ArcanePlugins/LevelledMobs',
+  },
+  chat: {
+    link: 'https://discord.gg/arcaneplugins-752310043214479462',
+  },
+  docsRepositoryBase: 'https://github.com',
+  footer: {
+    text: 'Last Update: Dec. 07, 2023. Translated By TecoStudio.',
+  },
+}
+
+export default config
