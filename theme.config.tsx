e2494b8431ce165c485b5c51b389669415d79692
JianyueHugo <valor@jianyuelab.cc>
2023-12-20 10:21:35 +0900

    test 2

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>LevelledMobs WIKI</span>,
+  logo: <span>TecoStudio WIKI</span>,
   project: {
     link: 'https://github.com/ArcanePlugins/LevelledMobs',
   },
